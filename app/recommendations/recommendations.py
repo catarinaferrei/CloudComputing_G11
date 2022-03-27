8e5dd75ddfb12351a99f0a9cdf9f126ee54c2a87
FilipeRM <filiperosamartins3@gmail.com>
2022-03-27 21:48:46 +0100

    alterações no ficheiro recommendations

--- app/recommendations/recommendations.py
+++ app/recommendations/recommendations.py
@@ -4,25 +4,13 @@ from turtle import color
 from flask import Flask, render_template
 import grpc
 
-
-
-#from protofbuf cliente import ....
-#from protobuf cars import ....
-#from ... import UserStub, CarsStub, UserRequest, CarsRequest
-
+from user_pb2 import UserRequest
+from user_pb2_grpc import UserStub
 
 app = Flask(__name__)
 
 #user_host = os.getenv("USER_HOST", "localhost") ?*?
 #cars_host = os.getenv("CARS_HOST", "localhost") ?*?
-
-#with open("client.key", "rb") as fp:
-#    client_key = fp.read()
-#with open("client.pem", "rb") as fp:
-#    client_cert = fp.read()
-#with open("ca.pem", "rb") as fp:
-#    ca_cert = fp.read()
-#creds = grpc.ssl_channel_credentials(ca_cert, client_key, client_cert)
 
 user_channel = grpc.insecure_channel(f"{user_host}:50051")
 user_client = UserStub(user_channel)
@@ -34,9 +22,6 @@ cars_client = CarsStub(cars_channel)
 @app.route("/recomendations/<userId>")
 def render_homepage():
 
-    #user = UserInfo(userId)
-    #listCars = Cars(user.preferences)
-
     user_request = UserRequest(
         user_id=userId
     )
@@ -44,14 +29,14 @@ def render_homepage():
         user_request
     )
 
-    cars_request = CarsRequest(
+    #cars_request = CarsRequest(
         #color=user_response.color, brand=user_response.brand ......
-    )
-    cars_response = cars_client.GetListOfCars(
-        cars_request
-    )
+    #)
+    #cars_response = cars_client.GetListOfCars(
+        #cars_request
+    #)
 
     return render_template(
-        "carsRecommendations.html",
-        listCarsView = cars_response.list,
+        "carsRecommendations.html"#,
+        #listCarsView = cars_response.list,
     )
