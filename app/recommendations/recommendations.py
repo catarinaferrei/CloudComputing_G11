fffc16291cc5f8c43e86d35bdc40188b0a903770
FilipeRM <filiperosamartins3@gmail.com>
2022-03-27 22:17:57 +0100

    recommendatios fn

--- app/recommendations/recommendations.py
+++ app/recommendations/recommendations.py
@@ -9,18 +9,18 @@ from user_pb2_grpc import UserStub
 
 app = Flask(__name__)
 
-#user_host = os.getenv("USER_HOST", "localhost") ?*?
-#cars_host = os.getenv("CARS_HOST", "localhost") ?*?
+user_host = os.getenv("USER_HOST", "localhost")
+#cars_host = os.getenv("CARS_HOST", "localhost")
 
-user_channel = grpc.insecure_channel(f"{user_host}:50051")
+user_channel = grpc.insecure_channel(f"{user_host}:50055")
 user_client = UserStub(user_channel)
 
-cars_channel = grpc.insecure_channel(f"{cars_host}:50051")
-cars_client = CarsStub(cars_channel)
+#cars_channel = grpc.insecure_channel(f"{cars_host}:50051")
+#cars_client = CarsStub(cars_channel)
 
 
 @app.route("/recomendations/<userId>")
-def render_homepage():
+def render_homepage(userId):
 
     user_request = UserRequest(
         user_id=userId
@@ -28,7 +28,7 @@ def render_homepage():
     user_response = user_client.GetPrefferedSpecs(
         user_request
     )
-
+    
     #cars_request = CarsRequest(
         #color=user_response.color, brand=user_response.brand ......
     #)
@@ -36,7 +36,4 @@ def render_homepage():
         #cars_request
     #)
 
-    return render_template(
-        "carsRecommendations.html"#,
-        #listCarsView = cars_response.list,
-    )
+    return user_response   
