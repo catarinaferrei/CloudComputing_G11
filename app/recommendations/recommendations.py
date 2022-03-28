a32055623bcff73a3abe6655ae666712e5033a80
FilipeRM <filiperosamartins3@gmail.com>
2022-03-28 23:50:03 +0100

    alterações ao dockerFIle e docker_yml

--- app/recommendations/recommendations.py
+++ app/recommendations/recommendations.py
@@ -1,11 +1,10 @@
 import os
-from turtle import color
 
 from flask import Flask, render_template
 import grpc
 
 from user_pb2 import UserRequest
-from user_pb2_grpc import UserStub
+from user_pb2_grpc import UsersStub
 
 app = Flask(__name__)
 
@@ -13,21 +12,21 @@ user_host = os.getenv("USER_HOST", "localhost")
 #cars_host = os.getenv("CARS_HOST", "localhost")
 
 user_channel = grpc.insecure_channel(f"{user_host}:50055")
-user_client = UserStub(user_channel)
+user_client = UsersStub(user_channel)
 
 #cars_channel = grpc.insecure_channel(f"{cars_host}:50051")
 #cars_client = CarsStub(cars_channel)
 
 
-@app.route("/recomendations/<userId>")
+@app.route("/recommendations/<userId>")
 def render_homepage(userId):
 
-    user_request = UserRequest(
-        user_id=userId
-    )
-    user_response = user_client.GetPrefferedSpecs(
-        user_request
-    )
+    #user_request = UserRequest(
+    #    user_id=userId
+    #)
+    #user_response = user_client.GetPrefferedSpecs(
+    #    user_request
+    #)
     
     #cars_request = CarsRequest(
         #color=user_response.color, brand=user_response.brand ......
@@ -36,4 +35,6 @@ def render_homepage(userId):
         #cars_request
     #)
 
-    return user_response   
+    #return user_response
+    return {'UserId':userId} 
+       
