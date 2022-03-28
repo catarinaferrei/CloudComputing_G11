a85ea5315f858e3a23b247af31fb18ed12bdcd0c
JQueimado <jpqueimado@gmail.com>
2022-03-28 17:47:21 +0100

    add required methods to CarRepairService

--- app/car_repair/app/car_repair_service.py
+++ app/car_repair/app/car_repair_service.py
@@ -5,19 +5,27 @@ import grpc
 from grpc_interceptor import ExceptionToStatusInterceptor
 from grpc_interceptor.exceptions import NotFound
 
+import car_repair_service_pb2_grpc
+
 PORT = "5001"
 
 #Service
-class CarRepairService( recommendations_pb2_grpc.RecommendationsServicer ):
-    def repair(self, request, context):
-        if request.category not in books_by_category:
-            raise NotFound("Category not found")
+class CarRepairService( ):
+    def Add(self, request, context):
+        ##TODO: Add car to repair to database and set status WAITING_CHECKUP
+        return AddResponse(car_id=1)
 
-        books_for_category = books_by_category[request.category]
-        num_results = min(request.max_results, len(books_for_category))
-        books_to_recommend = random.sample(books_for_category, num_results)
+    def GetRepairStatus ( self, request, context ):
+        ##TODO: Read status from database and deliver
+        return StatusResponse(car_id=1, status = CarRepairStatus.WAITING_CHECKUP)
 
-        return RecommendationResponse(recommendations=books_to_recommend)
+    def UpdateStatus ( self, request, context ):
+        ##TODO: Update car status in database
+        return StatusUpdateResponse( car_id=1 ) )
+
+    def Delete( self, request, context ):
+        ##TODO: Delete car repair request
+        return DeleteRepairRespose( car_id=1 )
 
 
 ##__RUN__##
@@ -26,8 +34,9 @@ def serve():
     server = grpc.server(
         futures.ThreadPoolExecutor(max_workers=10), interceptors=interceptors
     )
-    recommendations_pb2_grpc.add_RecommendationsServicer_to_server(
-        RecommendationService(), server
+
+    car_repair_service_pb2_grpc.add_CarRepairServiceServicer_to_server(
+        CarRepairService(), server
     )
 
     server.add_insecure_port("[::]:"+PORT)
