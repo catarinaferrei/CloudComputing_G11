2007e310648ce5111f983afdbfa3cdc5b6a028d0
FilipeRM <filiperosamartins3@gmail.com>
2022-03-29 21:33:17 +0100

    Recommendations-preparado para receber os outros servicos

--- app/recommendations/recommendations.py
+++ app/recommendations/recommendations.py
@@ -6,35 +6,43 @@ import grpc
 from user_pb2 import UserRequest
 from user_pb2_grpc import UsersStub
 
+#from cars_pb2 import CarsRequest
+#from cars_pb2_grpc import CarsStub
+
 app = Flask(__name__)
 
 user_host = os.getenv("USER_HOST", "localhost")
-#cars_host = os.getenv("CARS_HOST", "localhost")
-
 user_channel = grpc.insecure_channel(f"{user_host}:50055")
 user_client = UsersStub(user_channel)
 
-#cars_channel = grpc.insecure_channel(f"{cars_host}:50051")
+#cars_host = os.getenv("CARS_HOST", "localhost")
+#cars_channel = grpc.insecure_channel(f"{cars_host}:5000")
 #cars_client = CarsStub(cars_channel)
-
 
 @app.route("/recommendations/<userId>")
 def render_homepage(userId):
 
-    #user_request = UserRequest(
-    #    user_id=userId
-    #)
-    #user_response = user_client.GetPrefferedSpecs(
-    #    user_request
-    #)
+    user_request = UserRequest(
+        user_id=userId
+    )
+    user_response = user_client.Preferences(
+        user_request
+    )
     
     #cars_request = CarsRequest(
-        #color=user_response.color, brand=user_response.brand ......
+        #car_id = user_response
+        #region = user_response.region
+        #price = user_response.price
+        #year = user_response.year
+        #manufacturer = user_response.manufacturer
+        #model = user_response.model
+        #condition = user_response.condition
+        #car_id = 1
     #)
-    #cars_response = cars_client.GetListOfCars(
+    #cars_response = cars_client.CarSearch(
         #cars_request
     #)
 
-    #return user_response
-    return {'UserId':userId} 
+    return user_response
+    #return cars_response
        
